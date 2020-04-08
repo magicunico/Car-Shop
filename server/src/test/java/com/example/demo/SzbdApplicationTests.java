f8d4a3abcfe70ef402fa8fcc61b5652a82926f56
weroniquue <weronica97@gmail.com>
2020-04-08 22:36:13 +0200

    dodanie profilu do testu

--- server/src/test/java/com/example/demo/SzbdApplicationTests.java
+++ server/src/test/java/com/example/demo/SzbdApplicationTests.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class SzbdApplicationTests {
-//
-//    @Test
-//    public void contextLoads() {
-//    }
-//
-//}
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+public class SzbdApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
